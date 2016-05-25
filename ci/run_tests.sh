e06fcb43aa6e6c10f4761f1921dbf71b3b01643e
Joshua Carp <jm.carp@gmail.com>
2016-05-24 21:27:56 -0400

    Fix unit tests and concourse typos.

---
+++ ci/run_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e -x
+
+export GOPATH=$(pwd)/gopath
+export PATH=$PATH:$GOPATH/bin
+
+go get github.com/tools/godep
+
+cd gopath/src/github.com/18F/aws-broker
+
+cp secrets-test.yml secrets.yml
+
+godep get
+godep go build ./...
+godep go test ./...
