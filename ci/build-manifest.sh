337f24f41aca77881c85176a9fced373deedc4f6
Joshua Carp <jm.carp@gmail.com>
2016-05-26 19:11:42 -0400

    Template catalog and secrets with spiff.

--- ci/build-manifest.sh
+++ ci/build-manifest.sh
@@ -2,10 +2,36 @@
 
 set -e -x
 
-cat $BASE_FILE > $OUTPUT_FILE
+# Copy application to output
+cp -r aws-broker-app/* built
 
-cat << EOF >> $OUTPUT_FILE
+# Append environment variables to manifest
+cat << EOF >> built/manifest.yml
 env:
   DB_URL: `terraform output -state=$STATE_FILE -module=rds_internal rds_host`
   DB_PORT: `terraform output -state=$STATE_FILE -module=rds_internal rds_port`
 EOF
+
+# Build secrets for merging into templates
+cat << EOF > built/credentials.yml
+meta:
+  aws_broker:
+    subnet_group: $RDS_SUBNET_GROUP
+    security_group: $RDS_SECURITY_GROUPS
+  shared_mysql:
+    name: $RDS_SHARED_MYSQL_NAME
+    username: $RDS_SHARED_MYSQL_USERNAME
+    password: $RDS_SHARED_MYSQL_PASSWORD
+    url: `terraform output -state=$STATE_FILE -module=rds_shared_mysql rds_host`
+    port: `terraform output -state=$STATE_FILE -module=rds_shared_mysql rds_port`
+  shared_postgres:
+    name: $RDS_SHARED_POSTGRES_NAME
+    username: $RDS_SHARED_POSTGRES_USERNAME
+    password: $RDS_SHARED_POSTGRES_PASSWORD
+    url: `terraform output -state=$STATE_FILE -module=rds_shared_postgres rds_host`
+    port: `terraform output -state=$STATE_FILE -module=rds_shared_postgres rds_port`
+EOF
+
+# Merge secrets into templates
+spiff merge aws-broker-app/secrets-template.yml built/credentials.yml > built/secrets.yml
+spiff merge aws-broker-app/catalog-template.yml built/credentials.yml > built/catalog.yml
