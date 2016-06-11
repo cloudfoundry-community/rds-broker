f2017673d405b0d563731c26a6e02763009af744
Joshua Carp <jm.carp@gmail.com>
2016-06-11 11:08:03 -0400

    Delete existing app and service before test.

--- ci/run-smoke-tests.sh
+++ ci/run-smoke-tests.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-set -e
+set -e -u
 
 cf login -a $CF_API_URL -u $CF_DEPLOY_USERNAME -p $CF_DEPLOY_PASSWORD -o $CF_ORGANIZATION -s $CF_SPACE
 
+# Clean up existing app and service if present
+cf delete -f smoke-tests-$SERVICE_PLAN
+cf delete-service -f rds-smoke-tests-$SERVICE_PLAN
+
+# Create service
 cf create-service aws-rds $SERVICE_PLAN rds-smoke-tests-$SERVICE_PLAN
 
+# Write manifest
 cat << EOF > aws-broker-app/ci/smoke-tests/manifest.yml
 ---
 applications:
@@ -18,6 +24,7 @@ applications:
   - rds-smoke-tests-$SERVICE_PLAN
 EOF
 
+# Wait until service is available
 while true; do
   if OUT=`cf push -f aws-broker-app/ci/smoke-tests/manifest.yml -p aws-broker-app/ci/smoke-tests` ; then
     break
@@ -29,5 +36,6 @@ while true; do
   sleep 90
 done
 
+# Clean up app and service
 cf delete -f smoke-tests-$SERVICE_PLAN
 cf delete-service -f rds-smoke-tests-$SERVICE_PLAN
