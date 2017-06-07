f6e05a70460adb1c69eb1e58a295f6fb0a5e2900
Joshua Carp <jm.carp@gmail.com>
2017-06-07 13:31:57 -0400

    Use instance profile instead of user credentials.

--- ci/update-broker.sh
+++ ci/update-broker.sh
@@ -4,7 +4,7 @@ set -e
 
 cf login -a $CF_API_URL -u $CF_DEPLOY_USERNAME -p $CF_DEPLOY_PASSWORD -o $CF_ORGANIZATION -s $CF_SPACE
 
-BROKER_URL=https://`cf app $BROKER_NAME | grep urls: | sed 's/urls: //'`
+BROKER_URL=https://`cf app $BROKER_NAME | grep "routes: " | awk '{print $2}'`
 
 # Create or update service broker
 if ! cf create-service-broker $BROKER_NAME $AUTH_USER $AUTH_PASS $BROKER_URL ; then
