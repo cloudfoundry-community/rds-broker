7d4d5e515aa65bef5407320a1d26c81ae8895915
Tammer Saleh <tammer@superorbit.al>
2020-02-25 18:34:12 +0000

    This is one of a multi-phase terraform apply process.
    1. Align terraform with the current state of the world. (done)
    2. Then create resource groups with name_prefix and a create_before_destory lifecycle. (this commit)
    3. Move the existing RDS instances to those new resource groups.
    4. Finally, we remove the old resource groups.
    
    Once that's done, we can proceed with the RDS upgrade across staging and
    production.

--- ci/terraform/rds_module/parameter_group.tf
+++ ci/terraform/rds_module/parameter_group.tf
@@ -1,66 +1,66 @@
-# resource "aws_db_parameter_group" "recreatable_parameter_group_postgres" {
-#   count = "${var.rds_db_engine == "postgres" ? 1 : 0}"
-#
-#   name_prefix = "${var.rds_parameter_group_name != "" ?
-#     var.rds_parameter_group_name :
-#     "${replace("${var.stack_description}-${var.rds_db_name}", "/[^a-zA-Z-]+/", "-")}"}"
-#
-#   family = "${var.rds_parameter_group_family}"
-#
-#   parameter {
-#     name  = "log_connections"
-#     value = "1"
-#   }
-#
-#   parameter {
-#     name  = "log_disconnections"
-#     value = "1"
-#   }
-#
-#   parameter {
-#     name  = "log_hostname"
-#     value = "0"
-#   }
-#
-#   parameter {
-#     name  = "log_statement"
-#     value = "ddl"
-#   }
-#
-#   parameter {
-#     name         = "rds.force_ssl"
-#     value        = "${var.rds_force_ssl}"
-#     apply_method = "pending-reboot"
-#   }
-#
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
-#
-# resource "aws_db_parameter_group" "recreatable_parameter_group_mysql_with_prefix" {
-#   count = "${var.rds_db_engine == "mysql" ? 1 : 0}"
-#
-#   name_prefix = "${var.rds_parameter_group_name != "" ?
-#     var.rds_parameter_group_name :
-#     "${replace("${var.stack_description}-${var.rds_db_name}", "/[^a-zA-Z-]+/", "-")}"}"
-#
-#   family = "${var.rds_parameter_group_family}"
-#
-#   parameter {
-#     name  = "general_log"
-#     value = 1
-#   }
-#
-#   parameter {
-#     name  = "log_output"
-#     value = "FILE"
-#   }
-#
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+resource "aws_db_parameter_group" "recreatable_parameter_group_postgres" {
+  count = "${var.rds_db_engine == "postgres" ? 1 : 0}"
+
+  name_prefix = "${var.rds_parameter_group_name != "" ?
+    var.rds_parameter_group_name :
+    "${replace("${var.stack_description}-${var.rds_db_name}", "/[^a-zA-Z-]+/", "-")}"}"
+
+  family = "${var.rds_parameter_group_family}"
+
+  parameter {
+    name  = "log_connections"
+    value = "1"
+  }
+
+  parameter {
+    name  = "log_disconnections"
+    value = "1"
+  }
+
+  parameter {
+    name  = "log_hostname"
+    value = "0"
+  }
+
+  parameter {
+    name  = "log_statement"
+    value = "ddl"
+  }
+
+  parameter {
+    name         = "rds.force_ssl"
+    value        = "${var.rds_force_ssl}"
+    apply_method = "pending-reboot"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_db_parameter_group" "recreatable_parameter_group_mysql_with_prefix" {
+  count = "${var.rds_db_engine == "mysql" ? 1 : 0}"
+
+  name_prefix = "${var.rds_parameter_group_name != "" ?
+    var.rds_parameter_group_name :
+    "${replace("${var.stack_description}-${var.rds_db_name}", "/[^a-zA-Z-]+/", "-")}"}"
+
+  family = "${var.rds_parameter_group_family}"
+
+  parameter {
+    name  = "general_log"
+    value = 1
+  }
+
+  parameter {
+    name  = "log_output"
+    value = "FILE"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
 resource "aws_db_parameter_group" "parameter_group_postgres" {
   count = "${var.rds_db_engine == "postgres" ? 1 : 0}"
